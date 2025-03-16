58d8ffd8691ba6383c1f335e81fc983d670493b8
cdondi <clivedondi@hotmail.com>
2025-03-16 14:18:05 -0500

    Completed model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -67,5 +67,5 @@ if __name__ == "__main__":
         train_data, test_data
     )
 
-    # modeltrainer = ModelTrainer()
-    # print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
